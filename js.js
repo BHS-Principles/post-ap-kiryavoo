9c9342b690f2646402e13a56f7c68d8c31c87ffe
kiryavoo <2033195@apps.nsd.org>
2025-05-20 17:30:07 +0000

    feeding family

---
+++ js.js
@@ -0,0 +1,12 @@
+
+var List=["Dhruv is smart",
+"All other things",
+"Kirill"
+];
+
+var BTN=document.getElementById("mybutton")
+
+
+for(var i=0;i<List.length;i++){
+    alert("Check me out"+List[i])
+}
